538bf1f1c5e68043371349445b9f80599aca3270
Ana <anas.valenciae@autonoma.edu.co>
2024-07-16 11:56:30 -0500

    Ejemplo MODBUS RTU con librería funcional

--- lib/examples/RTU/masterSync/masterSync.ino
+++ lib/examples/RTU/masterSync/masterSync.ino
@@ -6,22 +6,16 @@
   (c)2020 Alexander Emelianov (a.m.emelianov@gmail.com)
   https://github.com/emelianov/modbus-esp8266
 */
-#include <Arduino.h>
+
 #include <ModbusRTU.h>
-//#include <SoftwareSerial.h>
-//Tratar de dejar el EN automático
+#include <SoftwareSerial.h>
 
 #define SLAVE_ID 1
-#define FIRST_REG 0 //FUNCIONANDO COMO OFFSET
-#define REG_COUNT 6
-#define EN 25
+#define FIRST_REG 0
+#define REG_COUNT 2
 
+SoftwareSerial S(D2, D1);
 ModbusRTU mb;
-
-bool cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
-  Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
-  return true;
-}
 
 bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Callback to monitor errors
   if (event != Modbus::EX_SUCCESS) {
@@ -32,15 +26,11 @@ bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Callb
 }
 
 void setup() {
-  
   Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1);
-  mb.begin(&Serial1, EN);
-  mb.master(); //Inicializar el maestro
-
+  S.begin(9600, SWSERIAL_8N1);
+  mb.begin(&S);
+  mb.master();
 }
-
-//bool coils[20];
 
 void loop() {
   uint16_t res[REG_COUNT];
@@ -48,10 +38,10 @@ void loop() {
     mb.readHreg(SLAVE_ID, FIRST_REG, res, REG_COUNT, cb); // Send Read Hreg from Modbus Server
     while(mb.slave()) { // Check if transaction is active
       mb.task();
-     // delay(10);
+      delay(10);
     }
     Serial.println(res[0]);
     Serial.println(res[1]);
   }
-  delay(5000);
+  delay(1000);
 }
