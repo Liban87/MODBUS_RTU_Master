4c14c0f4df1bb921990c730d77bdb966a2a8b3cf
Liban87 <lialbertoarias@proton.me>
2024-07-16 10:54:32 -0500

    Envio manual trama MODBUS ejemplo read Holding regs, funcional.

--- src/main.cpp
+++ src/main.cpp
@@ -7,54 +7,47 @@
   https://github.com/emelianov/modbus-esp8266
 */
 #include <Arduino.h>
-#include <ModbusRTU.h>
+//#include <ModbusRTU.h>
 //#include <SoftwareSerial.h>
 
 #define SLAVE_ID 1
 #define FIRST_REG 0
 #define REG_COUNT 22
-#define EN 21
+#define EN 25
+#define GP   0xA001 //generating polinomial
 
-//SoftwareSerial S(D2, D1);
-ModbusRTU mb;
-
-bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Callback to monitor errors
-  if (event != Modbus::EX_SUCCESS) {
-    Serial.print("Request result: 0x");
-    Serial.print(event, HEX);
-  }
-  return true;
+uint16_t Hregs[REG_COUNT];
+void ReadHreg(uint8_t addr, uint16_t offset){
+  char msg[8];
+  msg[0] = addr;
+  msg[1] = 0x03;
+  msg[2] = (uint8_t)offset;
+  msg[3] = offset>>8;
+  msg[4] = 0x00;
+  msg[5] = 0x06;
+  msg[6] = 0xC5;
+  msg[7] = 0xC8;
+  // for(int i = 0; i<8;i++){
+  //   Serial1.write(msg[i]);
+  // }
+  digitalWrite(EN,HIGH);
+  Serial1.write(msg,8);
+  vTaskDelay(9);
+  digitalWrite(EN,LOW);
 }
 
 void setup() {
 
-  Serial.begin(9600);
- #if defined(ESP8266)
-  S.begin(9600, SWSERIAL_8N1);
-  mb.begin(&S);
- #elif defined(ESP32)
-  Serial1.begin(9600, SERIAL_8N1);
-  //mb.setBaudrate(9600);
-  mb.begin(&Serial1);
-  //mb.setInterFrameTime(40000);
- #else
-  Serial1.begin(9600, SERIAL_8N1);
-  mb.begin(&Serial1);
-  mb.setBaudrate(9600);
- #endif
-  mb.master();
+  Serial.begin(115200);
+  Serial1.begin(9600);
   pinMode(EN,OUTPUT);
   digitalWrite(EN,LOW);
 }
 
-uint16_t Hregs[REG_COUNT];
 void loop() {
-  digitalWrite(EN,HIGH);
-  mb.readHreg(SLAVE_ID, FIRST_REG, Hregs, REG_COUNT, cb); // Send Read Hreg from Modbus Server
-  mb.task();
-  vTaskDelay(25);
-  digitalWrite(EN,LOW);
-  float frec=(float)(Hregs[13]<<16|Hregs[12]);
-  //Serial.println(frec);
+  ReadHreg(0x01,0x0000);
+  //Serial1.write('a');
+  Serial.flush();
+  Serial.println("a");
   delay(5000);
 }
